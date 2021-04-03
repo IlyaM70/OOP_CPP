ef47c7f1601798348658045659019d73c00679a2
Ilya <iliya.merkulov@mail.ru>
2021-04-03 21:27:23 +0500

    Operators overloading

--- Intro/Intro/Source.cpp
+++ Intro/Intro/Source.cpp
@@ -59,11 +59,42 @@ public:
 
 	//Operators
 
-	void operator=(const Point& other)
+	Point& operator=(const Point& other)
 	{
 		this->x = other.x;
 		this->y = other.y;
 		cout << "CopyAssignment: " << this << endl;
+		return *this;
+	}
+	Point& operator+=(const Point& other)
+	{
+		/*this->x += other.x;
+		this->y += other.y;*/
+		this->set_x(this->x + other.x);
+		this->set_y(this->y + other.y);
+		return *this;
+	}
+	// Increment/Decrement
+	Point& operator++() //prefix increment
+	{
+		this->x++;
+		this->y++;
+		return *this;
+	}
+	Point operator++(int) //Postfix increment
+	{
+		Point old = *this;
+		this->x++;
+		this->y++;
+		return old;
+	}
+
+
+	Point& operator()(double x, double y)
+	{
+		set_x(x);
+		set_y(y);
+		return *this;
 	}
 
 	//methods
@@ -72,9 +103,11 @@ public:
 	{
 		cout << "X=" << x << tab << "Y=" << y << endl;
 	}
-	double distance()
+	double distance(const Point& other)
 	{
-		double distance = sqrt(pow(x, 2) + pow(y, 2));
+		double x_distance = this->x - other.x;
+		double y_distance = this->y - other.y;
+		double distance = sqrt(pow(x_distance, 2) + pow(y_distance, 2));
 			return distance;
 	}
 };
@@ -85,10 +118,21 @@ public:
 
 //#define STRUCT
 //#define CONSTRUCT
+//#define DISTANCE
+//#define ASSIGNMENT_CHECK
+//#define STREAMS
 
-//functions
+//function declarations
 
 double distance(Point A, Point B);
+
+Point operator+(const Point& left, const Point& right);
+
+ostream& operator<<(ostream& os, const Point& obj);
+istream& operator>>(istream& is, Point& obj);
+
+bool operator==(const Point& left, const Point& right);
+bool operator !=(const Point& left, const Point& right);
 
 
 void main()
@@ -132,15 +176,73 @@ A.set_y(3);*/
 	E.print();
 #endif // CONSTRUCT
 
+#ifdef DISTANCE
 	Point A(2.5, 3.5);
 	cout << "точка А" << endl;
 	A.print();
-	cout << "Расстояние до точки "<<A.distance() << endl;
+	cout << "Расстояние до точки " << A.distance(0) << endl;
 
 	Point B(2, 3);
 	cout << "точка B" << endl;
 	B.print();
-	cout<< "Расстояние между точками " << distance(A, B)<<endl;
+	cout << "Расстояние между точками " << distance(A, B) << endl;
+#endif // DISTANCE
+
+#ifdef ASSIGNMENT_CHECK
+	int a, b, c;
+	a = b = c = 0;
+
+	Point A, B, C;
+	A = B = C = Point(2, 3);
+	A.print();
+	B.print();
+	C.print();
+#endif // ASSIGNMENT_CHECK
+	
+#ifdef STREAMS
+	int a = 2;
+	int b = 3;
+	int c = a + b;
+
+	Point A(2, 3);
+	Point B(3, 4);
+	//Point C = A + B;
+	//C.print();
+	//A += B;
+	//A.print();
+	//A++;
+	//A.print();  
+
+
+
+	cout << A << endl;
+	cout << B << endl;
+	cout << "Введите координаты точки "; cin >> A;
+	cout << "Вы ввели " << A << endl;
+#endif // STREAMS
+
+	Point A(2, 3);
+	Point B(3, 4);
+	/*if (A == B)
+	{
+		cout << "Точки равны" << endl;
+	}
+	else
+	{
+		cout << "Точки разные" << endl;
+	}*/
+	//cout << (A == B) << endl;
+	cout << (A != B) << endl;
+
+	cout << A << endl;
+	/*A.set_x(33);
+	A.set_y(44);*/
+	A(33, 44);
+	cout << A << endl;
+
+
+
+
 
 }
 
@@ -149,4 +251,37 @@ double distance(Point A, Point B)
 	double distance = sqrt(pow((A.get_x() - B.get_x()), 2) + pow((A.get_y() - B.get_y()), 2));
 
 	return distance;
+}
+Point operator+(const Point& left, const Point& right)
+{
+	Point result;
+	result.set_x(left.get_x() + right.get_x());
+	result.set_y(left.get_y() + right.get_y());
+	return result;
+}
+ostream& operator<<(ostream& os, const Point& obj)
+{
+	os << "X= " << obj.get_x() << tab << "Y= " << obj.get_y();
+	return os;
+}
+istream& operator>>(istream& is,  Point& obj)
+{
+	double x, y;
+	is >> x >> y;
+	obj.set_x(x);
+	obj.set_y(y);
+	return is;
+}
+
+bool operator==(const Point& left, const Point& right)
+{
+	/*if (left.get_x() == right.get_x() && left.get_y() == right.get_y())
+		return true;
+	else
+		return false;*/
+	return left.get_x() == right.get_x() && left.get_y() == right.get_y();
+}
+bool operator !=(const Point& left, const Point& right)
+{
+	return!(left == right);
 }
